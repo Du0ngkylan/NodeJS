016e3371aa0a651c6dbee880863b1bbb89ef4dc1
Du0ngkylan <duongxuanmai.k55tt@gmail.com>
2019-03-04 18:02:31 +0700

    Add files via upload

---
+++ student_manager_system/sms-db-manager/tests/sms-db-manager-test/test.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+// Node.js modules.
+const spawn = require('child_process').spawn;
+const path = require('path');
+
+
+function test() {
+  var process = spawn(
+    path.join(__dirname, '\\build\\Release\\goyo-db-manager-test.exe'));
+
+  process.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+    
+  process.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+};
+
+test();
