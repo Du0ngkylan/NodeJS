8bc82e841c8c90205eb43fda2c76e56a5186f293
Du0ngkylan <duongxuanmai.k55tt@gmail.com>
2019-03-23 21:03:22 +0700

    Delete goyo-window-controller.js

--- student_manager_system/sms-ui/lib/goyo-window-controller.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  windowHandler: require('./window-controller/window-handler'),
-  viewMode: require('./window-controller/view-mode'),
-  BookrackViewWindowSet: require('./window-controller/bookrackview-windowset'),
-  TreeViewWindowSet: require('./window-controller/tree-view-controller'),
-  AlbumWindowSet: require('./window-controller/album-windowset'),
-};
